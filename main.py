ed38f57e06b3f0a202f46bd245def85458eae938
xdddddnah-cell <xdddddnah@gmail.com>
2026-07-04 01:09:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ import os
 TOKEN = os.environ.get('TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
-# هذا السطر هو الحل! سيقوم بحذف الـ Webhook القديم قبل بدء البوت
+# هذا السطر ضروري جداً لإنهاء أي تعليق سابق
 bot.delete_webhook(drop_pending_updates=True)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, "تم الاستلام!")
 
-print("البوت يعمل الآن...")
 bot.infinity_polling()
